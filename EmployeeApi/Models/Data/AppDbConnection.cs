3e3fb553a9b8266826fe870ec698296538edef04
tanveerratul <tanvirratul18@gmail.com>
2026-08-21 20:23:16 +0600

    chore: AppDbConnection Created

---
+++ EmployeeApi/Models/Data/AppDbConnection.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using EmployeeApi.Models;
+
+namespace EmployeeApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Employee> Employees => Set<Employee>();
+}
